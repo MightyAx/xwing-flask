c55743332f8658b75c0e1445814060b734bd7b50
Alexie Staffer <MightyAx@users.noreply.github.com>
2017-07-30 08:07:53 +0100

    It's got style!

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
-@app.route("/")
-def helloworld():
-    return hello("World")
-    
-@app.route("/<username>")
-def hello(username):
-    return "Hello %s!" % username 
+@app.route('/')
+@app.route('/<username>')
+def hello(username=None):
+    return render_template('hello.html', name=username)
