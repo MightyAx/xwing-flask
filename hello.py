350f747ad46e3cc1ebb1a12b0834c04dd4ffb3aa
Alexie Staffer <MightyAx@users.noreply.github.com>
2017-07-30 08:34:19 +0100

    shoutout!

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<username>')
 def hello(username=None):
     return render_template('hello.html', name=username)
+
+@app.route('/shoutout', methods=['POST'])
+def shout_out():
+  return redirect(url_for('hello', username=request.form['name']))
