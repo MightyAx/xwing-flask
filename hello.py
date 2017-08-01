74d3beaf145bce8581ba1988ebe43e13e82bcf5d
Alexie Staffer <MightyAx@users.noreply.github.com>
2017-08-01 22:22:01 +0100

    database activity

--- hello.py
+++ hello.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
+import os, redis, datetime
+
 app = Flask(__name__)
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 @app.route('/')
 @app.route('/<username>')
 def hello(username='World'):
-#To-do:get list of greetings from database
-  return render_template('hello.html', name=username)
+  keys = r.lrange('greetings', 0, -1)
+  messages = []
+  for key in keys:
+    messages.append(dict(name = r.hget(key, 'name').decode("utf-8"), message = r.hget(key, 'message').decode("utf-8")))
+  return render_template('hello.html', name = username, messages = messages)
 
-@app.route('/shoutout', methods=['POST'])
+@app.route('/feedback', methods=['POST'])
 def post_greeting():
-#To-do: Save to database.
+  greeting_id = r.incr('next_greeting_id')
+  r.hmset('greeting:%s' % greeting_id, {'name':request.form['name'], 'message':request.form['message'], 'timestamp':datetime.datetime.now()})
+  r.lpush('greetings', 'greeting:%s' % greeting_id)
   return redirect(url_for('hello', username=request.form['name']))
