69f8ca686f9bf868fdb100052ab54fe0557c3099
Alexie Staffer <MightyAx@users.noreply.github.com>
2017-08-07 23:45:13 +0100

    Security callbacks

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
-import os, redis, datetime
+import os, redis, datetime, flask_login
 
 app = Flask(__name__)
 r = redis.from_url(os.environ.get("REDIS_URL"))
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 @app.route('/')
 def main():
@@ -24,3 +26,33 @@ def post_feedback():
   r.hmset('greeting:%s' % greeting_id, {'name':request.form['name'], 'message':request.form['message'], 'timestamp':datetime.datetime.now()})
   r.lpush('greetings', 'greeting:%s' % greeting_id)
   return redirect(url_for('feedback', username=request.form['name']))
+
+class User(flask_login.UserMixin):
+    id = ""
+    nickname = ""
+    email = ""
+
+@login_manager.user_loader
+def user_loader(id):
+    if not r.exists('user:%s' % id)
+        return
+    
+    user = User()
+    user.id = id
+    user.nickname = r.hget('user:%s' % id, 'nickname').decode("utf-8")
+    user.email = r.hget('user:%s' % id, 'email').decode("utf-8")
+	user.hash = r.hget('user:%s' % id, 'hash')
+	return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+	if not r.exists('email:%s' % email)
+		return
+	
+	id = r.get('email:%s' % email)
+	user = user_loader(id)
+	
+	#user.is_authenticated = hash(request.form['pw']) == user.hash
+	return user
+	
