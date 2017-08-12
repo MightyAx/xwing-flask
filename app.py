fde87664421f2fb1beb83fe92d6ca629174e484f
Alexie Staffer <MightyAx@users.noreply.github.com>
2017-08-12 12:22:45 +0100

    Validation & Case Insensitivity
    Email Validation and Case Insensitivity

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, request, redirect, url_for
 from passlib.hash import pbkdf2_sha256 
-import os, redis, datetime, flask_login
+import os, redis, datetime, flask_login, re
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECURE_KEY")
@@ -32,7 +32,7 @@ def post_feedback():
 class User(flask_login.UserMixin):
     def __init__(self, id, email, nickname, hash):
         self.id = int(id)
-        self.email = email
+        self.email = email.lower()
         self.nickname = nickname
         self.hash = hash
 
@@ -41,6 +41,12 @@ class User(flask_login.UserMixin):
         id = int(id)
         return cls(id, r.hget('user:%s' % id, 'email'), r.hget('user:%s' % id, 'nickname'), r.hget('user:%s' % id, 'hash'))
         
+def isValidEmail(email):
+ if len(email) > 7:
+    if re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email) != None:
+        return True
+ return False
+ 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
@@ -48,7 +54,12 @@ def load_user(user_id):
 @login_manager.request_loader
 def request_loader(request):
     email = request.form.get('email')
+    if not email:
+        return
+
+    email = email.lower()
     id = r.zscore('users', email)
+
     if not id:
         return
     user = User.get(id)
@@ -62,21 +73,24 @@ def register(error = None):
 
 @app.route('/register', methods=['POST'])
 def create_user():
-	email = request.form['email']
-	user_id = r.zscore('users', email)
-	if user_id:
-		return redirect(url_for('register', error='Email already registered'))
-	
-	user_id = int(r.incr('next_user_id'))
-	nickname = request.form['nickname']
-	hash = pbkdf2_sha256.encrypt(request.form['password'], rounds=200000, salt_size=16)
-	
-	user = User(user_id, email, nickname, hash)
-	r.hmset('user:%s' % user.id, {'email': user.email, 'nickname': user.nickname, 'hash':user.hash})
-	r.zadd('users',  user.email, user.id)
+    email = request.form['email'].lower()
+    if not isValidEmail(email):
+        return redirect(url_for('register', error='Not a valid Email Address'))
+    
+    user_id = r.zscore('users', email)
+    if user_id:
+        return redirect(url_for('register', error='Email already registered'))
+    
+    user_id = int(r.incr('next_user_id'))
+    nickname = request.form['nickname']
+    hash = pbkdf2_sha256.encrypt(request.form['password'], rounds=200000, salt_size=16)
+    
+    user = User(user_id, email, nickname, hash)
+    r.hmset('user:%s' % user.id, {'email': user.email, 'nickname': user.nickname, 'hash':user.hash})
+    r.zadd('users',  user.email, user.id)
   
-	flask_login.login_user(user)
-	return redirect(url_for('main'))	
+    flask_login.login_user(user)
+    return redirect(url_for('main'))    
 
 @app.route('/login', methods=['GET'])
 @app.route('/login/<error>', methods=['GET'])
@@ -85,7 +99,7 @@ def login(error = None):
 
 @app.route('/login', methods=['POST'])
 def check_login():
-    email = request.form['email']
+    email = request.form['email'].lower()
     id = r.zscore('users', email)
     if id:
         user = User.get(id)
